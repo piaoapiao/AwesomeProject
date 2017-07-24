b69c2b7bf506f97fa428ede080f02bb6f9e2eda8
guodong <guodong@guodongdeMacBook-Pro.local>
2017-07-25 00:13:06 +0800

    test button field

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>1Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text >Shake your phone to open the developer menu.</Text>
+
+
+          <TextInput   
+             style={styles.style_pwd_input}                
+              placeholder='密码'  
+              numberOfLines={1} 
+               alignItems="center"
+              underlineColorAndroid={'transparent'}   
+              secureTextEntry={true}  
+              textAlign='right'  
+          />  
+
+
+          <View style = {styles.btn}>
+          </View>                              
+        
+        {/* <TextInput/> */}
+        {/* <Text> 123</Text> */}
       </View>
     );
   }
@@ -20,4 +38,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+     btn: {
+        width: 155,
+        marginLeft: -5,
+        marginRight: 5,
+        backgroundColor: '#23bfff',
+        height: 45,    
+        alignItems: 'stretch',            
+        borderRadius: 4,
+
+    },
+      style_pwd_input:{    
+      backgroundColor:'#fff',  
+      height:45,  
+  }, 
+  // redColor:{
+  //   textColor:redColor
+  // }
 });
