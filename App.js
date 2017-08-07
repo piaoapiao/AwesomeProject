616d322c6b4bdc26d2868c920b6bc173206dd450
guodong <guodong@guodongdeMacBook-Pro.local>
2017-08-07 09:53:07 +0800

    测试rn

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View,TextInput ,Image,Button,Alert} from 'react-native';
+import { StyleSheet, Text, View,TextInput ,Image,Button,Alert,WebView,Picker,Dimensions} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
 
 export default class App extends React.Component {
+
+
+     constructor(props: any ) {
+        super(props);
+        this.state = {language:'java'}
+    }
 
 //  onPressLearnMore()
 //  {
 //     console.log("click")
 //  }
+
+// var DEFAULT_URL = "https://www.baidu.com";
+
+//var url = "http://www.58.com";
+
+// var BGWASH = 'rgba(255,255,255,0.8)';
 
  onPressButton() {
     Alert.alert('You tapped the button!')
@@ -15,6 +29,14 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+
+        <Button
+            title = "click2"
+            color = "red"
+            onPress={this.onPressButton}
+            accessibilityLabel="Learn more2 about this purple button"
+         />
+
         <Text>1Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text >Shake your phone to open the developer menu.</Text>         
@@ -26,15 +48,7 @@ export default class App extends React.Component {
             color = "red"
             onPress={() => { Alert.alert('You tapped the button!')}}
             accessibilityLabel="Learn more about this purple button"
-         />
-
-
-         <Button
-            title = "click2"
-            color = "red"
-            onPress={this.onPressButton}
-            accessibilityLabel="Learn more about this purple button"
-         />
+         />      
 
           <TextInput   
              style={styles.style_pwd_input}                
@@ -43,12 +57,34 @@ export default class App extends React.Component {
                alignItems="center"
               underlineColorAndroid={'transparent'}   
               secureTextEntry={true}  
-              textAlign='right'  
+              textAlign='center'  
           />  
 
 
           <View style = {styles.btn}>
-          </View>                              
+          </View>  
+
+        <Picker style={{ marginLeft:7, marginRight:8, flex:1}}
+          selectedValue={this.state.language}
+          onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>          
+        <Picker.Item label="Java2" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>
+
+
+          <WebView
+           style = {{
+            backgroundColor: "white",
+            height: 200,
+            width:200,
+           }}
+            source = {{uri: "http://www.baidu.com"}}
+            javaScriptEnabled={true} 
+            domStorageEnabled={true} 
+            scalesPageToFit={false}  
+          />
+    
+
         
         {/* <TextInput/> */}
         {/* <Text> 123</Text> */}
@@ -78,6 +114,10 @@ const styles = StyleSheet.create({
       backgroundColor:'#fff',  
       height:45,  
   }, 
+ webView: {
+    // backgroundColor: BGWASH,
+    height: 350,
+  },    
   // redColor:{
   //   textColor:redColor
   // }
